9bd73641cc3697ea709b0a17a12a417310fc9b40
Aleksey Bobrovskyi <leshabobrovsky99@gmail.com>
2021-12-23 19:03:41 +0200

    add

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,23 +1,26 @@
 import axios from 'axios';
 
 const key = '24499282-2c88a00bf4afff93913de8ef0';
-const URL = "https://pixabay.com/api/";
 
-export const fetchImages = async (searchQuery, currentPage) => {
-  try {
-    const result = await axios.get(URL, {
-      params: {
-        key: key,
-        q: searchQuery,
-        page: currentPage,
-        per_page: 12,
-        image_type: "photo",
-        orientation: "horizontal",
-      },
-    });
-    console.log(result);
-    return result;
-  } catch (error) {
-    console.error(error);
-  }
+const instance = axios.create({
+  baseURL: 'https://pixabay.com/api/',
+  params: {
+    key: key,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    per_page: 12,
+  },
+});
+
+const fetchImages = (page, query) => {
+  return instance.get('/', {
+    params: {
+      page,
+      q: query
+    },
+  });
+};
+
+export const Api = {
+  fetchImages,
 };
--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,10 @@ import SearchBar from "./SearchBar/SearchBar";
 import Modal from "./Modal/Modal";
 import Button from "./Button/Button";
 import Spinner from "./Spinner/Spinner";
-import * as API from "../Api/Api";
+import {Api} from "../Api/Api";
 import "../../src/App.css";
 import styles from "../components/Modal/Modal.module.css";
+import Loader from "react-loader-spinner";
 
 class App extends Component {
   state = {
@@ -17,48 +18,87 @@ class App extends Component {
     isModalOpen: false,
     isLoading: false,
     error: null,
+  
+
     
   };
 
-  componentDidUpdate(_, prevState) {
-    if (prevState.searchQuery !== this.state.searchQuery) {
-      this.onSearch();
+  componentDidUpdate(_,prevState) {
+    // const prevQuery=prevState.searchQuery;
+    // const nextQuery = this.state.searchQuery;
+    // if (!nextQuery){
+    //   return;
+    // }
+   
+    
+    // if (prevQuery !== nextQuery) {
+    //   this. onSearch();
+    //   this.setState({
+    //     images: [],
+    //     currentPage: 1,
+        
+    //   });
+  const {currentPage, isLoading, searchQuery} = this.state;
+  if(prevState.searchQuery !== searchQuery || (isLoading && prevState.currentPage < currentPage)) {this.onSearch();} 
+  }
+ // if (prevState.searchQuery !== this.state.searchQuery) {
+    //  this.onSearch();
+   // } 
+
+  
+  async onSearch() {
+    try {
+      
+      const { currentPage ,searchQuery} = this.state;
+      const result = await Api.fetchImages(currentPage, searchQuery);
+      console.log(result)
+      if (!result.data.hits.length) {
+        alert("Sorry we can't find anything(");
+        return;
+      }
+      this.setState((prevState) =>({
+                images: [...prevState.images, ...result.data.hits],
+                isLoading: false,
+                error: false,
+                //currentPage: prevState.currentPage + 1,
+              }))
+    } catch (error) {
+      this.setState({isLoading: false, error})
+    console.log(error);
     }
   }
+  // onSearch = () => {
+  //   const { searchQuery, currentPage } = this.state;
+  //   this.setState({ isLoading: true });
 
-  onSearch = () => {
-    const { searchQuery, currentPage } = this.state;
-    this.setState({ isLoading: true });
+  //  Api.fetchImages(searchQuery, currentPage)
+  //     .then(({ data }) => {
+  //       if (!data.hits.length){
+  //           return alert("Impossible to load the pictures")
+  //         }
+  //       this.setState((prevState) =>({
+  //         images: [...prevState.images, ...data.hits],
+  //         isLoading: false,
+  //         error: false,
+  //         currentPage: prevState.currentPage + 1,
+  //       }))
+          
 
-   API.fetchImages(searchQuery, currentPage)
-      .then(({ data }) => {
         
-        this.setState((prevState) =>{ 
-          if (!data.hits.length){
-            return alert("Impossible to load the pictures")
-          }
-return ({
-          images: [...prevState.images, ...data.hits],
-          isLoading: false,
-          error: false,
-          currentPage: prevState.currentPage + 1,
-        });
-      })
-        
-      })
+  //     })
       
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        window.scrollTo({
-          top: document.documentElement.scrollHeight,
-          behavior: "smooth",
-        });
+  //     .catch((error) => {
+  //       console.log(error);
+  //     })
+  //     .finally(() => {
+  //       window.scrollTo({
+  //         top: document.documentElement.scrollHeight,
+  //         behavior: "smooth",
+  //       });
 
-        this.setState({ isLoading: false });
-      });
-  };
+  //       this.setState({ isLoading: false });
+  //     });
+  // };
 
   toggleModal = () => {
     this.setState(({ isModalOpen }) => ({ isModalOpen: !isModalOpen }));
@@ -71,27 +111,30 @@ return ({
   onFormSubmit = (item) => {
     this.setState({
       searchQuery: item,
-      images: [],
+      // images: [],
       currentPage: 1,
     });
   };
+  loadMore = ()=>{
+    this.setState(({currentPage})=>({currentPage:currentPage+1,isLoading:true}));
+  }
 
   render() {
-    const { isModalOpen, error, images, largeImageURL, isLoading } = this.state;
+    const { isModalOpen, images, largeImageURL, isLoading } = this.state;
+    console.log(this.state.searchQuery)
 
     return (
       <div className="App">
         <SearchBar onSubmit={this.onFormSubmit} />
-        {error && <p>Impossible to load the pictures!</p>}
         <Gallery images={images} onClick={this.getLargeUrl} />
         {isModalOpen && (
           <Modal onClose={this.toggleModal}>
             <img className={styles.Modal} src={largeImageURL} alt="" />
           </Modal>
         )}
-        {isLoading && <Spinner />}
+        {isLoading && <Loader />}
         {images.length >= 12 && !isLoading && (
-          <Button onClick={this.onSearch} />
+          <Button onClick={this.loadMore} />
         )}
       </div>
     );
